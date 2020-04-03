f5096852e64422df2f9114b9bb73162508879dfe
scursa <57352455+scursa@users.noreply.github.com>
2020-04-03 23:04:20 +0200

    Export morance #61
    fixes #61

--- Anagrafe/morance/gest_morance.php
+++ Anagrafe/morance/gest_morance.php
@@ -110,8 +110,9 @@ $first = ($pag - 1) * $x_pag;
 echo "<h2>".$jsonObj->{$lang."Morance"}[0]."</h2>";//Villaggio Ntchangue
 echo "<h3>".$jsonObj->{$lang."Morance"}[1]."</h3>";//Elenco Morance
 echo "<a href='ins_moranca.php'>".$jsonObj->{$lang."Morance"}[2]."</a><br><br>";//Aggiungi una nuova moranca
+echo "<a href='export_moranca.php'>export su excel</a><br><br>";
 
-//Select option per la scelta della zona
+//Select option per la scelta della zona ed export
 echo "<form action='gest_morance.php' method='POST'><br>";
 echo   $jsonObj->{$lang."Morance"}[3].": <select name='cod_zona'>";
 $result = $conn->query("SELECT * FROM zone");
@@ -130,6 +131,7 @@ for($i=0;$i<$nz;$i++)
 echo "</select>";
 echo " <input type='submit' value='".$jsonObj->{$lang."Morance"}[4]."'>";//Conferma
 echo " </form>";
+
 
 // ordinamento su campi (11/3/2020) A.C.
 if (!isset($_POST['ord'])) 
