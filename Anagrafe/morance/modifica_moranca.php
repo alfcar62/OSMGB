dc72cecf72294b165cccdd111f107091a02fb862
Alfonso Carlone <alfonso.carlone@gmail.com>
2020-04-11 19:20:52 +0200

    correzione errori
    correzione errori in inserimento storico morance

--- Anagrafe/morance/modifica_moranca.php
+++ Anagrafe/morance/modifica_moranca.php
@@ -31,41 +31,65 @@ $cod_zona=$_POST["cod_zona"];
    $query = "SELECT * FROM morance WHERE ID='$id_moranca' ";
 
    $query = "SELECT ";
-   $query .= " m.id, m.nome, m.id_mor_zona, m.cod_zona,";
+   $query .= " m.id, m.nome, m.id_mor_zona, m.cod_zona, m.id_osm, ";
    $query .= " m.data_inizio_val";
    $query .= " FROM morance m";
    $query .= " WHERE m.id =  $id_moranca";
 
    $result = $conn->query($query);
-   if (!$result )
+   if (!$result)
         throw new Exception($conn->error);
 
    $row = $result->fetch_array();
 
-   $query  = "INSERT INTO morance_sto (";
-   $query .= "TIPO_OP,";
-   $query .= "ID_MORANCA,";
-   $query .= "ID_MOR_ZONA,";
-   $query .= "NOME, ";  
-   $query .= "COD_ZONA,";
-   $query .= "DATA_INIZIO_VAL,";
-   $query .= "DATA_FINE_VAL) ";
-   $query .= "VALUES (";
-   $query .= "'MODIFICA',";
-   $query .= "$row[id],";
-   $query .= "$row[id_mor_zona],";
-   $query .= "'$row[nome]',";
-   $query .= "'$row[cod_zona]',";
-   $query .= "'$row[data_inizio_val]',";
-   $query .= "'$dataOggi')";
+   if (($nome !=  $row['nome']) ||
+      ($cod_zona != $row['cod_zona']) ||
+      ($id_osm != $row['id_osm']))
+     $upd = true;
+   else
+     $upd = false;
+  
+   if ($upd)
+   { 
+	 $tipo_operazione= "MOD (";
+
+     if ($nome !=  $row['nome'])
+	     $tipo_operazione.="-NOME-";
+
+     if ($cod_zona != $row['cod_zona'])
+	     $tipo_operazione.="-ZONA-";
+
+     if ($id_osm != $row['id_osm'])
+	 	$tipo_operazione.="-ID_OSM-";
+
+     $tipo_operazione.=")";
+
+     $query  = "INSERT INTO morance_sto (";
+     $query .= "TIPO_OP,";
+     $query .= "ID_MORANCA,";
+     $query .= "ID_MOR_ZONA,";
+     $query .= "NOME, ";  
+     $query .= "COD_ZONA,";
+     $query .= "ID_OSM,";
+     $query .= "DATA_INIZIO_VAL,";
+     $query .= "DATA_FINE_VAL) ";
+     $query .= "VALUES (";
+     $query .= "'".$tipo_operazione."',";
+     $query .= $row['id'].",";
+     $query .= $row['id_mor_zona'].",";
+     $query .= "'$row[nome]',";
+     $query .= "'$row[cod_zona]',";
+     $query .= $row['id_osm'].",";
+     $query .= "'$row[data_inizio_val]',";
+     $query .= "'$dataOggi')";
 
    echo $query;
 
-   $result = $conn->query($query);
+     $result = $conn->query($query);
 
-   if (!$result)
+    if (!$result)
         throw new Exception($conn->error);
-
+    
     $query=  "UPDATE morance SET ";
     $query.= "NOME='$nome', "  ; 
     $query.= "COD_ZONA='$cod_zona', "  ; 
@@ -74,24 +98,24 @@ $cod_zona=$_POST["cod_zona"];
 
    echo $query;
 
-   $result = $conn->query($query);
-   if (!$result) 
+    $result = $conn->query($query);
+    if (!$result) 
         throw new Exception($conn->error);
  
-   $conn->commit(); 
-   $conn->autocommit(TRUE);
-   $conn->close();
+    $conn->commit(); 
+    $conn->autocommit(TRUE);
+    $conn->close();
+   }// upd
   }
- catch ( Exception $e )
-  {
+  catch ( Exception $e )
+   {
     $conn->rollback(); 
     $conn->autocommit(TRUE); // i.e., end transaction
 	$conn->close();
     $mymsg =  "Errore nella modifica  della moranca: ";
 	$mymsg .=  $conn->error; 
 	EchoMessage($mymsg, "gest_morance.php?pag=$pag");
-  }
- 
+   }
  EchoMessage("Modifica moranca effettuata correttamente", "gest_morance.php?pag=$pag");
   
 ?>
