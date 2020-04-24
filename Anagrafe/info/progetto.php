d7eb34865d31d43105e6ed03360cf1e6cde007e8
Alfonso Carlone <alfonso.carlone@gmail.com>
2020-04-24 21:45:12 +0200

    aggiunto link a github

--- Anagrafe/info/progetto.php
+++ Anagrafe/info/progetto.php
@@ -11,18 +11,20 @@ unsetPag(basename(__FILE__));
 ?>
 <div align="center">
 <h1>  Il progetto <br></h1>
+<br><br>
+<h3>Guarda il video di presentazione del progetto </h3>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/lj0iqdUjjAA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+<br><br>
 <h3>Apri la pagina Wiki ed entra nel mondo di questo progetto con un semplice click.
 </h3>
 
 <A HREF="https://wiki.openstreetmap.org/wiki/OsmGuineaBissau_Avogadro" target='new'> <IMG SRC="../img/wikiosm.png"  ALT="la nostra pagina Wiki"></A>
-<h3>Guarda il video di presentazione del progetto </h3>
 
+<br><br>
+<h3>Apri la pagina Github con il codice Open Source</h3>
+<A HREF="https://github.com/alfcar62/OSMGB" target='new'> <IMG SRC="../img/github.jpg"  ALT="codice del progetto"></A>
 
-<iframe width="560" height="315" src="https://www.youtube.com/embed/lj0iqdUjjAA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-
-<!--<div id="divVideo"><video poster="../img/poster.png"  controls width="50%" height="50%" >
-    <source src="../img/video.mp4" type="video/mp4">
-</video></div>-->
 </div>
 
 </body>
