e702793375500709bd387055d724b224b50fd46a
FerraiuoloP <58466635+FerraiuoloP@users.noreply.github.com>
2020-05-12 14:41:25 +0200

    Aggiunta Matricola
    Non è presente uno storico matricola ma in storico_persona è possibile salvare la matricola senza salvare la data di inizio e di fine.Per come è adesso l'applicazione se si decidesse di modificare la matricola è possibile visualizzare quella/e vecchie mentre se si modificasse la data di inizio e fine è possibile visualizzare solo quella più recente.Per gestire la fk matricola_stud presente in persona ho aggiunto il constraint ON UPDATE CASCADE ON DELETE SET NULL in modo da poter modificare/eliminare la matricola in studenti e di conseguenza svolgere la stessa operazione in persone

--- Anagrafe/persone/insert_persona.php
+++ Anagrafe/persone/insert_persona.php
@@ -15,70 +15,122 @@ $data_nascita=$_POST["data_nascita"];
 $id_casa=$_POST["id_casa_nuova"];
 $cod_ruolo=$_POST["id_ruolo_nuovo"];
 $sesso=$_POST["sesso"];
+$matricola=$_POST['matricola'];
+$inizio_matricola=$_POST['inizio_matricola'];
 $data_odierna = date("y/m/d");
 
 try 
- {
-   $conn->query("START TRANSACTION"); //inizio transazione
+{
+    $conn->query("START TRANSACTION"); //inizio transazione
 
-// se la  casa ha già un capo famiglia, non posso scegliere come ruolo capo famiglia
-  if ($cod_ruolo == 'CF')
-   {
-    $query  =  " SELECT count(pc.id) as cont FROM casa c, pers_casa pc ";
-    $query .=  " WHERE pc.id_casa = c.id ";
-    $query .=  " AND c.id =". $id_casa;
-    $query .=  " AND pc.cod_ruolo_pers_fam = 'CF'";
-//	echo $query;
+    // se la  casa ha già un capo famiglia, non posso scegliere come ruolo capo famiglia
+    if ($cod_ruolo == 'CF')
+    {
+        $query  =  " SELECT count(pc.id) as cont FROM casa c, pers_casa pc ";
+        $query .=  " WHERE pc.id_casa = c.id ";
+        $query .=  " AND c.id =". $id_casa;
+        $query .=  " AND pc.cod_ruolo_pers_fam = 'CF'";
+        //	echo $query;
 
-    $result = $conn->query($query);
+        $result = $conn->query($query);
 
-	if (!$result)
-     {
-      $msg_err = "Errore select n.2";
-      throw new Exception($conn->error);
-     }
-    $row = $result->fetch_array();
-    if ($row['cont']>0) 
-	 {
-       $msg_err = "Esiste un capo famiglia: selezionare altro ruolo";
-       throw new Exception($msg_err);
-     }
-   }
+        if (!$result)
+        {
+            $msg_err = "Errore select n.2";
+            throw new Exception($conn->error);
+        }
+        $row = $result->fetch_array();
+        if ($row['cont']>0) 
+        {
+            $msg_err = "Esiste un capo famiglia: selezionare altro ruolo";
+            throw new Exception($msg_err);
+        }
+    }
 
 
-   $query="select  max(id) as max_id_pers from persone";
-   $result=$conn->query($query);
-   $row=$result->fetch_array();
-   $id_pers=$row["max_id_pers"]+1;
-   $result->free();
+    $query="select  max(id) as max_id_pers from persone";
+    $result=$conn->query($query);
+    $row=$result->fetch_array();
+    $id_pers=$row["max_id_pers"]+1;
+    $result->free();
+    if(isset($matricola) and $matricola!=null){
 
-   $query="INSERT INTO persone (id, nominativo, sesso, data_nascita,data_inizio_val) values ($id_pers,'$nominativo','$sesso','$data_nascita','$data_odierna')";
-   $result = $conn->query($query);
-  // echo $query."<br>";
+        $query  =  " SELECT count(s.matricola) as count from studenti s "; //verifico se la matricola è già esistente in quanto deve essere univoca,se si lancio un exception
+        $query .=  " WHERE s.matricola ='$matricola'";
+        //	echo $query;
 
-   $query2="INSERT INTO pers_casa (id_pers, id_casa, cod_ruolo_pers_fam, data_inizio_val)";
-   $query2 .= " VALUES ($id_pers, $id_casa, '$cod_ruolo', '$data_odierna')";
-   $result = $conn->query($query2);
-  // echo $query2."<br>";
-   if (!$result)
-     {
+        $result = $conn->query($query);
+
+
+        if (!$result)
+        {
+            $msg_err = "Errore count matricola";
+            throw new Exception($conn->error);
+        }
+        $row = $result->fetch_array();
+        
+        if ($row['count']>0) 
+        {
+            $msg_err = "Esiste già un altra persona con la stessa matricola: verificarne la correttezza";
+            throw new Exception($msg_err);
+        }
+        
+        $query="INSERT INTO studenti (matricola,data_inizio_val) values ('$matricola','$inizio_matricola')"; 
+        $result = $conn->query($query);
+        // echo $query."<br>";
+        if (!$result)
+        {
+            $msg_err = "Errore insert studenti".$query;
+            throw new Exception($conn->error);
+        }
+
+        $query="INSERT INTO persone (id, nominativo, sesso, data_nascita,data_inizio_val,matricola_stud) values ($id_pers,'$nominativo','$sesso','$data_nascita','$data_odierna','$matricola')";
+        $result = $conn->query($query);
+        // echo $query."<br>";
+        if (!$result)
+        {
+            $msg_err = "Errore insert persone";
+            throw new Exception($conn->error);
+        }
+
+
+    }
+
+    else{
+        $query="INSERT INTO persone (id, nominativo, sesso, data_nascita,data_inizio_val) values ($id_pers,'$nominativo','$sesso','$data_nascita','$data_odierna')";
+        $result = $conn->query($query);
+        // echo $query."<br>";
+        if (!$result)
+        {
+            $msg_err = "Errore insert persone";
+            throw new Exception($conn->error);
+        } 
+
+    }
+
+    $query2="INSERT INTO pers_casa (id_pers, id_casa, cod_ruolo_pers_fam, data_inizio_val)";
+    $query2 .= " VALUES ($id_pers, $id_casa, '$cod_ruolo', '$data_odierna')";
+    $result = $conn->query($query2);
+    // echo $query2."<br>";
+    if (!$result)
+    {
         $msg_err = "Errore insert pers_casa";
         throw new Exception($conn->error);
-     }
-	$conn->commit(); 
+    }
+    $conn->commit(); 
     $conn->autocommit(TRUE); // end transaction
-	$conn->close();
-  }
-  catch (Exception $e )
-   {
-     $conn->rollback(); 
-     $conn->autocommit(TRUE);	// end transaction
-	 $conn->close();
-//     echo "Errore in inserimento della persona";
-//	 echo $conn->error; 
-//	 echo "transazione con rollback";
-	 $mymsg = "Errore inserimento persona id=$id_pers " . $msg_err;
-     EchoMessage($mymsg, "gest_persone.php");
-   }
-   EchoMessage("Inserimento  persona id=$id_pers effettuato correttamente", "gest_persone.php");
+    $conn->close();
+}
+catch (Exception $e )
+{
+    $conn->rollback(); 
+    $conn->autocommit(TRUE);	// end transaction
+    $conn->close();
+    //     echo "Errore in inserimento della persona";
+    //	 echo $conn->error; 
+    //	 echo "transazione con rollback";
+    $mymsg = "Errore inserimento persona id=$id_pers " . $msg_err;
+    EchoMessage($mymsg, "gest_persone.php");
+}
+EchoMessage("Inserimento  persona id=$id_pers effettuato correttamente", "gest_persone.php");
 ?>
--- Anagrafe/persone/modifica_persona.php
+++ Anagrafe/persone/modifica_persona.php
@@ -12,7 +12,7 @@ require_once $util1;
 setup();
 isLogged("gestore");
 if (isset($_SESSION['pag_p']['pag_p']))
-   $pag=$_SESSION['pag_p']['pag_p'];
+    $pag=$_SESSION['pag_p']['pag_p'];
 unset($_SESSION['pag_c']);
 
 // salvo i nuovi valori
@@ -34,238 +34,344 @@ $id_pers_modifica=$_SESSION['id_persona_modifica'];
 $id_casa_new=$_POST['id_casa_modifica'];//casa pers modifica
 //echo "id_casa:$id_casa_new<br>";
 
+$matricola_new=$_POST['matricola'];//matricola pers modifica
+
+$inizio_matricola_new=$_POST['inizio_matricola'];//fine_matricola pers modifica
+
+$fine_matricola_new=$_POST['fine_matricola'];//inizio_matricola pers modifica
+
 try 
- {
- // $conn->query("START TRANSACTION"); //inizio transazione
+{
+    // $conn->query("START TRANSACTION"); //inizio transazione
 
-  //query per prendere i valori della persona pre-modifica
+    //query per prendere i valori della persona pre-modifica
 
-  $query  =  " SELECT p.nominativo,";
-  $query .=  " p.data_nascita,";
-  $query .=  " p.data_morte,";
-  $query .=  " p.data_inizio_val,";
-  $query .=  " p.sesso,";
-  $query .=  " c.id as id_casa,";
-  $query .=  " c.nome as nome_casa,";
-  $query .=  " pc.cod_ruolo_pers_fam as cod_ruolo,";
-  $query .=  " rpf.descrizione as desc_ruolo";
-  $query .=  " FROM persone p, pers_casa pc, casa c, ruolo_pers_fam rpf";
-  $query .= " WHERE p.id =$id_pers_modifica";
-  $query .= " AND pc.id_pers = p.id";
-  $query .= " AND c.id = pc.id_casa";
-  $query .= " AND pc.cod_ruolo_pers_fam = rpf.cod";
+    $query  =  " SELECT p.nominativo,";
+    $query .=  " p.data_nascita,";
+    $query .=  " p.data_morte,";
+    $query .=  " p.data_inizio_val,";
+    $query .=  " p.sesso,";
+    $query .=  " c.id as id_casa,";
+    $query .=  " c.nome as nome_casa,";
+    $query .=  " pc.cod_ruolo_pers_fam as cod_ruolo,";
+    $query .=  " rpf.descrizione as desc_ruolo,";
+    $query .=  " p.matricola_stud as matricola,";
+    $query .=  " s.data_inizio_val as inizio_matricola,";
+    $query .=  " s.data_fine_val as fine_matricola";
+    $query .=  " FROM persone p, pers_casa pc, casa c, ruolo_pers_fam rpf,studenti s";
+    $query .= " WHERE p.id =$id_pers_modifica";
+    $query .= " AND pc.id_pers = p.id";
+    $query .= " AND c.id = pc.id_casa";
+    $query .= " AND pc.cod_ruolo_pers_fam = rpf.cod";
 
- // echo "q1 ".$query."<br>";
-  $result=$conn->query($query);
-  if (!$result)
-   {
-     $msg_err = "Errore select n.1";
-     throw new Exception($conn->error);
-   }
-  $row=$result->fetch_array();
+    // echo "q1 ".$query."<br>";
+    $result=$conn->query($query);
+    if (!$result)
+    {
+        $msg_err = "Errore select n.1";
+        throw new Exception($conn->error);
+    }
+    $row=$result->fetch_array();
 
-  $tipo_operazione="MOD (";
-  $casa_cambiata=false;
-  $ruolo_cambiato=false;
-  $nominativo_cambiato = false;
-  $data_nascita_cambiata = false;
-  $data_morte_cambiata = false;
+    $tipo_operazione="MOD (";
+    $casa_cambiata=false;
+    $ruolo_cambiato=false;
+    $nominativo_cambiato = false;
+    $data_nascita_cambiata = false;
+    $data_morte_cambiata = false;
+    $matricola_cambiata=false;
+    $nuova_matricola=false; //indica se la matricola precedentemente all'update non era presente.Utile per l'update della persona più avanti
+    $inizio_matricola_cambiato=false;
+    $fine_matricola_cambiato=false;
 
-  $sesso_old =$row['sesso']; 
-  $nome_casa_old =$row['nome_casa']; 
-  $desc_ruolo_old =$row['desc_ruolo']; 
-  $nominativo_old =$row['nominativo'];
-  $cod_ruolo_old = $row['cod_ruolo']; 
-  $data_nascita_old =  $row['data_nascita'];
-  $data_morte_old =  $row['data_morte'];
-  $id_casa_old = $row['id_casa'];
+    $sesso_old =$row['sesso']; 
+    $nome_casa_old =$row['nome_casa']; 
+    $desc_ruolo_old =$row['desc_ruolo']; 
+    $nominativo_old =$row['nominativo'];
+    $cod_ruolo_old = $row['cod_ruolo']; 
+    $data_nascita_old =  $row['data_nascita'];
+    $data_morte_old =  $row['data_morte'];
+    $id_casa_old = $row['id_casa'];
+    $matricola_old=$row['matricola'];
+    $inizio_matricola_old=$row['inizio_matricola'];
+    $fine_matricola_old=$row['fine_matricola'];
 
-  if($nominativo_new != $row['nominativo'])
-   {
-    $tipo_operazione.="-nominativo-";
-    $nominativo_cambiato=true; 
-   }
-  
-  if($id_ruolo_modifica_new != $row['cod_ruolo'])
-   {
-    $tipo_operazione.="-ruolo-";
-    $ruolo_cambiato=true;
-//	echo $row['cod_ruolo'];
-//	echo "ruolo cambiato";
-   }
-  
-  $data_nascita =($row['data_nascita'] != '') ? $row['data_nascita']:"0000-00-00";
+    if($nominativo_new != $row['nominativo'])
+    {
+        $tipo_operazione.="-nominativo-";
+        $nominativo_cambiato=true; 
+    }
 
-  if($data_nascita_new != $data_nascita)
-   {
-    $tipo_operazione.="-data_nascita-";
-    $data_nascita_cambiata=true;
-   }
+    if($id_ruolo_modifica_new != $row['cod_ruolo'])
+    {
+        $tipo_operazione.="-ruolo-";
+        $ruolo_cambiato=true;
+        //	echo $row['cod_ruolo'];
+        //	echo "ruolo cambiato";
+    }
 
-   $data_morte =($row['data_morte'] != '') ? $row['data_morte']:"0000-00-00";
+    $data_nascita =($row['data_nascita'] != '') ? $row['data_nascita']:"0000-00-00";
 
-   if($data_morte_new != $data_morte)
-   {
-    $tipo_operazione.="-data_morte-";
-    $data_morte_cambiata=true;
-   }
+    if($data_nascita_new != $data_nascita)
+    {
+        $tipo_operazione.="-data_nascita-";
+        $data_nascita_cambiata=true;
+    }
 
+    $data_morte =($row['data_morte'] != '') ? $row['data_morte']:"0000-00-00";
 
-  if( $id_casa_new != 's' && $id_casa_new != $row['id_casa'])
-   {
-    $tipo_operazione.="-casa-";
-    $casa_cambiata=true;
-   }
-      
-  $tipo_operazione.=")";
-
-  $data_inizio_val=$row['data_inizio_val'];
-  $currentdate=date('Y/m/d');
+    if($data_morte_new != $data_morte)
+    {
+        $tipo_operazione.="-data_morte-";
+        $data_morte_cambiata=true;
+    }
 
 
-  // se la nuova casa ha gi� un capo famiglia, non posso scegliere come ruolo capo famiglia
-  if ($ruolo_cambiato  && $id_ruolo_modifica_new == 'CF')
-   {
-    $query  =  " SELECT count(pc.id) as cont FROM casa c, pers_casa pc ";
-    $query .=  " WHERE pc.id_casa = c.id ";
-    $query .=  " AND c.id =". $id_casa_new;
-    $query .=  " AND pc.cod_ruolo_pers_fam = 'CF'";
-//	echo $query;
+    if( $id_casa_new != 's' && $id_casa_new != $row['id_casa'])
+    {
+        $tipo_operazione.="-casa-";
+        $casa_cambiata=true;
+    }
+    if($matricola_new != $row['matricola'])
+    {
+        $tipo_operazione.="-matricola-";
+        $matricola_cambiata=true; 
+    }
+    if($inizio_matricola_new != $row['inizio_matricola'] && $inizio_matricola_new !="")
+    {
+        
+        $tipo_operazione.="-inizio_matricola-";
+        $inizio_matricola_cambiato=true; 
+    }
+    if($fine_matricola_new != $row['fine_matricola'] && $fine_matricola_new !="")
+    {
+        $tipo_operazione.="-fine_matricola-";
+        $fine_matricola_cambiato=true; 
+    }
 
-    $result = $conn->query($query);
 
-	if (!$result)
-     {
-      $msg_err = "Errore select n.2";
-      throw new Exception($conn->error);
-     }
-    $row = $result->fetch_array();
-    if ($row['cont']>0) 
-	 {
-       $msg_err = "Esiste un capo famiglia: selezionare altro ruolo";
-       throw new Exception($msg_err);
-     }
-   }
+    $tipo_operazione.=")";
 
-/*
+    $data_inizio_val=$row['data_inizio_val'];
+    $currentdate=date('Y/m/d');
+
+
+    // se la nuova casa ha gi� un capo famiglia, non posso scegliere come ruolo capo famiglia
+    if ($ruolo_cambiato  && $id_ruolo_modifica_new == 'CF')
+    {
+        $query  =  " SELECT count(pc.id) as cont FROM casa c, pers_casa pc ";
+        $query .=  " WHERE pc.id_casa = c.id ";
+        $query .=  " AND c.id =". $id_casa_new;
+        $query .=  " AND pc.cod_ruolo_pers_fam = 'CF'";
+        //	echo $query;
+
+        $result = $conn->query($query);
+
+        if (!$result)
+        {
+            $msg_err = "Errore select n.2";
+            throw new Exception($conn->error);
+        }
+        $row = $result->fetch_array();
+        if ($row['cont']>0) 
+        {
+            $msg_err = "Esiste un capo famiglia: selezionare altro ruolo";
+            throw new Exception($msg_err);
+        }
+    }
+
+    /*
 *** INSERT su persone_sto (vecchi valori)
 */
- $query="INSERT INTO persone_sto (";
- $query .= "  TIPO_OP, ";
- $query .= "  ID_PERSONA, ";
- $query .= "  NOMINATIVO, ";
- $query .= "  SESSO, ";
- $query .= "  DATA_NASCITA, DATA_MORTE,";
- $query .= "  ID_CASA, NOME_CASA,";
- $query .= "  COD_RUOLO_PERS_FAM, DESC_RUOLO_PERS_FAM,";
- $query .= "  DATA_INIZIO_VAL,DATA_FINE_VAL ";
- $query .= "  )";
- $query .= " VALUES(";
- $query .= "'$tipo_operazione',";
- $query .= $id_pers_modifica.",";
- $query .= "'".$nominativo_old."',";
- $query .= "'".$sesso_old."',";
+    $query="INSERT INTO persone_sto (";
+    $query .= "  TIPO_OP, ";
+    $query .= "  ID_PERSONA, ";
+    $query .= "  NOMINATIVO, ";
+    $query .= "  SESSO, ";
+    $query .= "  DATA_NASCITA, DATA_MORTE,";
+    $query .= "  ID_CASA, NOME_CASA,";
+    $query .= "  COD_RUOLO_PERS_FAM, DESC_RUOLO_PERS_FAM,";
+    $query .= "  DATA_INIZIO_VAL,DATA_FINE_VAL,MATRICOLA_STUD ";
+    $query .= "  )";
+    $query .= " VALUES(";
+    $query .= "'$tipo_operazione',";
+    $query .= $id_pers_modifica.",";
+    $query .= "'".$nominativo_old."',";
+    $query .= "'".$sesso_old."',";
 
- if ($data_nascita_new == "0000-00-00")
-    $query .= "NULL,";
- else
-    $query .= "'".$data_nascita_new."',";
-
- if ($data_morte_new == "0000-00-00")
-    $query .= "NULL,";
- else
-    $query .= "'".$data_morte_new."',";
-
- $query .= $id_casa_old .",";
- $query .= "'".$nome_casa_old."',";
- $query .= "'".$cod_ruolo_old."',";
- $query .= "'".$desc_ruolo_old."',";
- $query .= "'$data_inizio_val',";
- $query .= "'$currentdate')";
- 
-// echo "q2 ".$query."<br>";
- $result = $conn->query($query);
-
- if (!$result)
-  {
-     $msg_err = "Errore insert persone_sto";
-     throw new Exception($conn->error);
-  }
-
- $upd_pers      = false;
- $upd_pers_casa = false;
-
- if ($nominativo_cambiato || $data_nascita_cambiata || $data_morte_cambiata)
-       $upd_pers=true;
-
- if ($casa_cambiata || $ruolo_cambiato)
-      $upd_pers_casa = true;
-   
-
- if($upd_pers)
-   {
-    /*
-    *** UPDATE persone
-    */
-    $query= "UPDATE persone SET ";
-    $query .= "nominativo="."'".$nominativo_new."'";
-
-	if ($data_nascita_new == "0000-00-00")
-        $query .= ",data_nascita = NULL ";
-	else
-        $query .= ",data_nascita = '". $data_nascita_new . "'";
+    if ($data_nascita_new == "0000-00-00")
+        $query .= "NULL,";
+    else
+        $query .= "'".$data_nascita_new."',";
 
     if ($data_morte_new == "0000-00-00")
-        $query .= ",data_morte = NULL ";
-	else
-        $query .= ",data_morte= '". $data_morte_new . "' ";
+        $query .= "NULL,";
+    else
+        $query .= "'".$data_morte_new."',";
 
-    $query .= " where id= ".$id_pers_modifica;
-	
-	echo "q3 ".$query."<br>";
+    $query .= $id_casa_old .",";
+    $query .= "'".$nome_casa_old."',";
+    $query .= "'".$cod_ruolo_old."',";
+    $query .= "'".$desc_ruolo_old."',";
+    $query .= "'$data_inizio_val',";
+    $query .= "'$currentdate',";
+    $query .= "'$matricola_old')";
 
+    // echo "q2 ".$query."<br>";
     $result = $conn->query($query);
+
     if (!$result)
-     {
-      $msg_err = "Errore update persone";
-      throw new Exception($conn->error);
-     }
+    {
+        $msg_err = "Errore insert persone_sto";
+        throw new Exception($conn->error);
+    }
+
+    $upd_pers = false;
+    $upd_pers_casa = false;
+
+    if ($nominativo_cambiato || $data_nascita_cambiata || $data_morte_cambiata)
+        $upd_pers=true;
+
+    if($matricola_cambiata || $inizio_matricola_cambiato || $fine_matricola_cambiato)
+        $upd_matricola=true;
+    
+    if ($casa_cambiata || $ruolo_cambiato)
+        $upd_pers_casa = true;
+
+if($upd_matricola){
+if($matricola_cambiata and ($matricola_new!=null or $matricola_new!='')){//verifico se la matricola è già esistente in quanto deve essere univoca
+        $query  =  " SELECT count(s.matricola) as count from studenti s "; 
+        $query .=  " WHERE s.matricola ='$matricola_new'";
+        //	echo $query;
+
+        $result = $conn->query($query);
+       
+
+        if (!$result)
+        {
+            $msg_err = "Errore count matricola";
+            throw new Exception($conn->error);
+        }
+        $row = $result->fetch_array();
+        if ($row['count']>0) 
+        {
+            $msg_err = "Esiste già un altra persona con la stessa matricola: verificarne la correttezza";
+            throw new Exception($msg_err);
+        }
+}
+
+
+        if($matricola_old==null || $matricola_old==""){ //se la matricola è da inserire e non da modificare
+            $query= "INSERT INTO studenti(matricola,data_inizio_val,data_fine_val) ";
+            $query.="VALUES('$matricola_new'";
+            if ($inizio_matricola_new == "0000-00-00")
+                $query .= ",'NULL'";
+            else
+                $query .=",'$inizio_matricola_new'";
+            if ($fine_matricola_new == "0000-00-00")
+                $query .= ",'NULL'";
+            else
+                $query .=",'$fine_matricola_new')";
+
+            $result = $conn->query($query);
+            if (!$result)
+            {
+                $msg_err = "Errore insert studenti";
+                throw new Exception($conn->error);
+            }
+            $nuova_matricola=true;//variabile che indica che deve essere esserci anche la matricola nell'update
+        }else{// se c'è da fare un update o delete
+            if($matricola_new==null or $matricola_new==''){//se è da eliminare la matricola
+                $query ="DELETE from studenti where matricola='{$matricola_old}'";
+                $result = $conn->query($query);
+                if (!$result)
+                {
+                    $msg_err = "Errore delete studenti";
+                    throw new Exception($conn->error);
+                }
+            }else{//se è da modificare la matricola
+                $query= "UPDATE studenti SET ";
+                $query.="matricola="."'".$matricola_new."'";
+                $query.=",data_inizio_val="."'".$inizio_matricola_new."'";
+                $query.=",data_fine_val="."'".$fine_matricola_new."'";
+                $query .= " where matricola= '$matricola_old'";
+                $result = $conn->query($query);
+                if (!$result)
+                {
+                    $msg_err = "Errore update studenti";
+                    throw new Exception($conn->error);
+                }
+            }
+        }
+
+    }
+    
+    if($upd_pers || $nuova_matricola)//$nuova_matricola è settata a true in caso si dovesse fare l'update su persone(solo quando c'è una nuova matricola perchè negli altri casi grazie all' ON CASCADE UPDATE si aggiorna da sola) 
+    {
+        /*
+    *** UPDATE persone
+    */
+
+        $query= "UPDATE persone SET ";
+        $query .= "nominativo="."'".$nominativo_new."'";
+
+        if ($data_nascita_new == "0000-00-00")
+            $query .= ",data_nascita = NULL ";
+        else
+            $query .= ",data_nascita = '". $data_nascita_new . "'";
+
+        if ($data_morte_new == "0000-00-00")
+            $query .= ",data_morte = NULL ";
+        else
+            $query .= ",data_morte= '". $data_morte_new . "' ";
+        if($nuova_matricola)
+            $query .= ",matricola_stud= '". $matricola_new . "' ";
+        $query .= " where id= ".$id_pers_modifica;
+
+        echo "q3 ".$query."<br>";
+
+        $result = $conn->query($query);
+        if (!$result)
+        {
+            $msg_err = "Errore update persone";
+            throw new Exception($conn->error);
+        }
     }
     /*
     *** UPDATE pers_casa
     */
-	if ($upd_pers_casa)
-     {
-      $query="UPDATE pers_casa ";
-      $query=$query." SET cod_ruolo_pers_fam="."'".$id_ruolo_modifica_new. "'";
-      $query=$query.", id_casa=".$id_casa_new;
-	  $query=$query." WHERE pers_casa.id_pers=".$id_pers_modifica;
-   
-//      echo "q4 ".$query."<br>";;
-      $result = $conn->query($query);
-      if (!$result)
-       {
-        $msg_err = "Errore update pers_casa";
-        throw new Exception($conn->error);
-       }
-     }
+    if ($upd_pers_casa)
+    {
+        $query="UPDATE pers_casa ";
+        $query=$query." SET cod_ruolo_pers_fam="."'".$id_ruolo_modifica_new. "'";
+        $query=$query.", id_casa=".$id_casa_new;
+        $query=$query." WHERE pers_casa.id_pers=".$id_pers_modifica;
+
+        //      echo "q4 ".$query."<br>";;
+        $result = $conn->query($query);
+        if (!$result)
+        {
+            $msg_err = "Errore update pers_casa";
+            throw new Exception($conn->error);
+        }
+    }
     $conn->commit();
-	$conn->autocommit(TRUE);
+    $conn->autocommit(TRUE);
     $conn->close();
-  }//try
- catch ( Exception $e )
-  {
-	$conn->rollback(); 
+}//try
+catch ( Exception $e )
+{
+    $conn->rollback(); 
     $conn->autocommit(TRUE); // i.e., end transaction
-//	echo $conn->error; 
-	$conn->close();
-//	echo $msg_err;
+    //	echo $conn->error; 
+    $conn->close();
+    //	echo $msg_err;
     echo "Errore nella modifica della persona";
-	echo "transazione con rollback";
+    echo "transazione con rollback";
     $mymsg = "Modifica persona id=$id_pers_modifica " . $msg_err;
     EchoMessage($mymsg, "gest_persone.php?pag=$pag");
-  }
- EchoMessage("Modifica persona id=$id_pers_modifica effettuata correttamente", "gest_persone.php?pag=$pag");
+}
+EchoMessage("Modifica persona id=$id_pers_modifica effettuata correttamente", "gest_persone.php?pag=$pag");
 ?>
 
 
