dc9026c89d3bd04f7b40f625082cc1eaea63e61e
Alfonso Carlone <alfonso.carlone@gmail.com>
2020-05-16 10:02:04 +0200

    aggiunto menu statistiche

--- Anagrafe/stat/fasce.php
+++ Anagrafe/stat/fasce.php
@@ -11,6 +11,12 @@ isLogged();
 <body>
 <?php stampaNavbar(); 
 ?>
+<br>
+<a href='statistiche_det.php'> Dettaglio Statistiche <IMG SRC="../img/inserisci2.png"></a>
+&nbsp;&nbsp;
+<a href='statistiche_zona.php'>Statistiche per zona <i class="fa fa-pie-chart" aria-hidden="true"></i></a>
+<br>
+
 <?php
 $util = $config_path .'/../db/db_conn.php';
 require $util;
